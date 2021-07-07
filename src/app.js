87bd1d31628c65c4d772f48eeb3774ed0c1fe987
szatolokin <staszatolokin28@gmail.com>
2021-07-07 11:50:31 +0300

    Update nav-page-d (js) comp

--- src/app.js
+++ src/app.js
@@ -1,5 +1,33 @@
 import 'Styles/_app.scss'
 
-$(() => {
-	console.log('App start')
-})
+// nav-page-d
+;(() => {
+	$(() => {
+		const components = $('.nav-page-d')
+
+		components.each(function () {
+			const component = $(this)
+
+			const state = {
+				activeItem: component.find('.nav-page-d__item--active'),
+				
+				setActiveItem: item => {
+					state.activeItem.removeClass('nav-page-d__item--active')
+					state.activeItem = item
+					state.activeItem.addClass('nav-page-d__item--active')
+				},
+			}
+
+			const links = component.find('.nav-page-d__link')
+			
+			links.on('click', function (event) {
+				event.preventDefault()
+
+				const link = $(this)
+				const item = link.closest('.nav-page-d__item')
+
+				state.setActiveItem(item)
+			})
+		})
+	})
+})()
