aca76b84e65b90648cb6cdb90cf77fc1b884bb8c
Nikita <nikita.ableev@mail.ru>
2021-07-22 18:08:44 +0300

    Add sliders

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import 'Styles/_app.scss'
 
+import Swiper from 'swiper/bundle';
+
 // vars
 const BREAKPOINT = 1280
 const BREAKPOINT_MEDIA = matchMedia(`(min-width: ${BREAKPOINT}px)`)
@@ -205,36 +207,36 @@ const BREAKPOINT_MEDIA = matchMedia(`(min-width: ${BREAKPOINT}px)`)
 
 // mdl
 {
-	$(() => {
-		const triggers = [
-			'.review-card--modal',
-			'.button-modal',
-			'[data-mdl-open]',
-		].join(', ')
+  $(() => {
+    const triggers = [
+      '.review-card--modal',
+      '.button-modal',
+      '[data-mdl-open]',
+    ].join(', ')
 
-		const state = {
-			isActive: checkActive(),
-		}
+    const state = {
+      isActive: checkActive(),
+    }
 
     if (!state.isActive) {
       waitOpen()
     }
 
-		$(triggers).on('click', event => {
-			event.preventDefault()
-		})
+    $(triggers).on('click', event => {
+      event.preventDefault()
+    })
 
     function checkActive() {
       return $('.mdl').hasClass('mdl--active')
     }
 
-		function waitOpen() {
-			$(triggers).on('click', handleClick)
+    function waitOpen() {
+      $(triggers).on('click', handleClick)
 
       function handleClick() {
         console.log('open click');
 
-				$(triggers).off('click', handleClick)
+        $(triggers).off('click', handleClick)
 
         open()
       }
@@ -490,6 +492,61 @@ const BREAKPOINT_MEDIA = matchMedia(`(min-width: ${BREAKPOINT}px)`)
               slides.not(slide).removeClass('spray-page__slider-slide--active');
             }, 500)
           }
+        });
+      });
+    }
+  });
+}
+
+
+// slider
+{
+  $(window).on('load', () => { // ?
+    const slider = $('[data-slider-id]');
+
+    if (slider.length !== 0) {
+      slider.each(function () {
+        const slider_el = $(this);
+        const slider_id = slider_el.data('slider-id');
+        const slider_prev_id = slider_el.data('slider-prev');
+        const slider_next_id = slider_el.data('slider-next');
+        const slider_prev = $(`[data-slider-button="${slider_prev_id}"]`);
+        const slider_next = $(`[data-slider-button="${slider_next_id}"]`);
+
+        let slider_options = {
+          slidesPerView: 'auto',
+
+          spaceBetween: 30,
+
+          breakpoints: {
+            [BREAKPOINT]: {
+              spaceBetween: 30,
+            },
+          },
+        };
+
+        // switch (slider_id) {
+        //   case 1:
+        //     slider_options = {
+        //       ...slider_options,
+
+        //       breakpoints: {
+        //         [BREAKPOINT]: {
+        //           spaceBetween: 60,
+        //         },
+        //       },
+        //     }
+        //     break;
+
+        // }
+
+        const slider_swiper = new Swiper(slider_el[0], slider_options);
+
+        slider_prev.on('click', () => {
+          slider_swiper.slidePrev();
+        });
+        slider_next.on('click', () => {
+          slider_swiper.slideNext();
         });
       });
     }
