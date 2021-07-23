ecb545c58f5757a8df6fa0f5f2e79569a54866b4
Nikita <nikita.ableev@mail.ru>
2021-07-23 16:33:56 +0300

    Update consultation modal

--- src/app.js
+++ src/app.js
@@ -552,3 +552,45 @@ const BREAKPOINT_MEDIA = matchMedia(`(min-width: ${BREAKPOINT}px)`)
     }
   });
 }
+
+// test
+{
+  $(() => {
+    if ($('.file').length !== 0) {
+      const inputFile = $('.file__input');
+      const button = $('.file__label');
+      const filesContainer = $('.file__container');
+      let files = [];
+
+      inputFile.on('change', function() {
+        let newFiles = [];
+
+        for(let index = 0; index < inputFile[0].files.length; index++) {
+          let file = inputFile[0].files[index];
+          newFiles.push(file);
+          files.push(file);
+        }
+
+        newFiles.forEach(file => {
+          let fileElement = $(
+            `<div class=file__item><p class=file__name>${file.name}</p><div class=file__mark></div></div>`
+            );
+          fileElement.data('fileData', file);
+          filesContainer.append(fileElement);
+
+          fileElement.on('click', function(event) {
+            const target = $(event.target);
+            const fileMark = target.closest('.file__mark')
+            const indexToRemove = files.indexOf($(this).data('fileData'));
+
+            if (fileMark.length === 1) {
+              $(this).remove();
+              files.splice(indexToRemove, 1);
+            }
+          });
+        });
+      });
+    }
+  });
+}
+
